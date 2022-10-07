bb30d879ce0997eaae62d147158987b2058bda2b
jinwooooooo <ko440338@gmail.com>
2022-10-07 13:26:03 +0900

    (FEAN-124) feat : 주문 시 kafka Producer

---
+++ src/main/java/com/lotte/danuri/order/service/messagequeue/KafkaProducerServiceImpl.java
@@ -0,0 +1,33 @@
+package com.lotte.danuri.order.service.messagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lotte.danuri.order.model.dto.OrderDataDto;
+import com.lotte.danuri.order.model.entity.OrderData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducerServiceImpl implements KafkaProducerService{
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    @Override
+    public OrderDataDto send(String topic, OrderDataDto orderDataDto){
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "";
+        try{
+            jsonInString = mapper.writeValueAsString(orderDataDto);
+        } catch(JsonProcessingException ex){
+            ex.printStackTrace();
+        }
+
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("Kafka Producer sent data from the Order microservice" + orderDataDto);
+
+        return orderDataDto;
+    }
+}
