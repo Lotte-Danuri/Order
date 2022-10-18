9d37514ceb863328d62b2781cff45671d7f541b8
jinwooooooo <ko440338@gmail.com>
2022-10-18 14:55:10 +0900

    (FEAN-170) feat : KafkaConfig 파일 수정

--- src/main/java/com/lotte/danuri/order/config/KafkaProducerConfig.java
+++ src/main/java/com/lotte/danuri/order/config/KafkaProducerConfig.java
@@ -1,9 +1,11 @@
 package com.lotte.danuri.order.config;
 
+import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,11 +16,14 @@ import java.util.Map;
 
 @EnableKafka
 @Configuration
+@AllArgsConstructor
 public class KafkaProducerConfig {
+
+    private final Environment env;
     @Bean
     public ProducerFactory<String, String> producerFactory(){
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"43.200.203.135:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("danurikafka.ip"));
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
