f92e2f4fd9ab878d469be3c70104e9264c4463d3
jinwooooooo <ko440338@gmail.com>
2022-11-01 18:14:19 +0900

    (FEAN-220) feat : 주문 시 장바구니 삭제 Service 수정 (kafka)

--- src/main/java/com/lotte/danuri/order/service/messagequeue/KafkaProducerService.java
+++ src/main/java/com/lotte/danuri/order/service/messagequeue/KafkaProducerService.java
@@ -1,7 +1,9 @@
 package com.lotte.danuri.order.service.messagequeue;
 
 import com.lotte.danuri.order.model.dto.OrderDataDto;
+import com.lotte.danuri.order.model.dto.OrderHeaderDto;
 
 public interface KafkaProducerService {
     OrderDataDto send(String topic, OrderDataDto orderDataDto);
+    OrderHeaderDto send(String topic, OrderHeaderDto orderHeaderDto);
 }
